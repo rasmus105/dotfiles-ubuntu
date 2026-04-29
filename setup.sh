5eee4da1eccdcb87a55dfee2501b9ace29adbd97
Rasmus Kargo <rasmus105@protonmail.com>
2026-04-29 13:17:34 +0200

    avoid dependency on dotfiles installation directory

--- setup.sh
+++ setup.sh
@@ -11,8 +11,10 @@ export PATH="/home/linuxbrew/.linuxbrew/bin/:$PATH"
 echo "Installing packages with homebrew..."
 brew bundle # install all packages from Brewfile
 
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+
 echo "Stowing configuration files..."
 mkdir -p "$HOME/.config/" "$HOME/.local"
-stow --restow -d ~/dotfiles -t ~/.config config # stow config/ to ~/.config/
-stow --restow -d ~/dotfiles -t ~ home # stow home/ to ~/
-stow --restow -d ~/dotfiles -t ~/.local local # stow local/ to ~/.local/
+stow --restow -d "$SCRIPT_DIR" -t ~/.config config # stow config/ to ~/.config/
+stow --restow -d "$SCRIPT_DIR" -t ~ home # stow home/ to ~/
+stow --restow -d "$SCRIPT_DIR" -t ~/.local local # stow local/ to ~/.local/
